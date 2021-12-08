8c01918e93a00f2eb59fe544eea0d376ed9ebcc4
YYB <1196736083@qq.com>
2021-12-08 11:42:59 +0800

    项目初始化结构创建

---
+++ gulimall-product/src/test/java/com/yee/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yee.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
