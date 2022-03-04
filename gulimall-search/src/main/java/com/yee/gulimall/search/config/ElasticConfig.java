217e7acecb3d6a3270a5c79f86e4cee175e324be
YYB <1196736083@qq.com>
2022-03-04 10:25:18 +0800

    检索服务 -》 elasticsearch整合测试

---
+++ gulimall-search/src/main/java/com/yee/gulimall/search/config/ElasticConfig.java
@@ -0,0 +1,40 @@
+package com.yee.gulimall.search.config;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author YYB
+ */
+@Configuration
+public class ElasticConfig {
+
+    public static final RequestOptions COMMON_OPTIONS;
+
+    static {
+        RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+
+        COMMON_OPTIONS = builder.build();
+    }
+
+    @Bean
+    public ElasticsearchClient esRestClient() {
+        // Create the low-level client
+        RestClient restClient = RestClient.builder(
+                new HttpHost("211.159.224.38", 9200)).build();
+
+        // Create the transport with a Jackson mapper
+        ElasticsearchTransport transport = new RestClientTransport(
+                restClient, new JacksonJsonpMapper());
+
+        // And create the API client
+        return new ElasticsearchClient(transport);
+    }
+}
