b413dda3f541d298e62cbbfff21d456be036db9a
Yee <yyb990929@gmail.com>
2022-07-09 17:50:02 +0800

    将微服务配置放入nacos中

--- gulimall-product/src/main/java/com/yee/gulimall/product/web/ItemController.java
+++ gulimall-product/src/main/java/com/yee/gulimall/product/web/ItemController.java
@@ -8,6 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.concurrent.ExecutionException;
+
 /**
  * @author Yee
  */
@@ -18,7 +20,7 @@ public class ItemController {
     SkuInfoService skuInfoService;
 
     @GetMapping("/{skuId}.html")
-    public String skuItem(@PathVariable("skuId") Long skuId, Model model) {
+    public String skuItem(@PathVariable("skuId") Long skuId, Model model) throws ExecutionException, InterruptedException {
         SkuItemVO skuItemVO = skuInfoService.item(skuId);
         model.addAttribute("item", skuItemVO);
         return "item";
